1562ce9393a436bdd34df09488aa8d58f6b4f85b
FR <55557961+federueda@users.noreply.github.com>
2020-02-06 18:21:32 +0100

    Add files via upload

---
+++ src/unittest/python/contracts_tests.py
@@ -0,0 +1,23 @@
+from mockito import mock, verify
+import unittest
+
+#from models.provider import Provider
+
+#pr = Provider(1111,"TechTrends","Barranquilla",True,False)
+
+# Confirm expected data
+#pr.Change_Name("ComputersCo")
+#assert pr.Provider_Name == "ComputersCo"
+
+##assert pr.Show_data() == "Provider_ID 1111\nProvider_Name ComputersCo\nProvider_Address = Barranquilla\nLocal_Company True\nQuality_Cert False"
+
+
+#from contracts import contracts
+
+#class ContractsTest(unittest.TestCase):
+    #def test_should_issue_(self):
+        #out = mock()
+
+        #contracts(out)
+
+        #verify(out).write("Hello world of Python\n")

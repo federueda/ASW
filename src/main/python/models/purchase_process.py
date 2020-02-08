084ff9ed8fda2db90b5e681417dd0f86e7fa5fb3
Federico Rueda <federico.rueda@gmail.com>
2020-02-08 01:55:44 +0100

    updates classes files

---
+++ src/main/python/models/purchase_process.py
@@ -0,0 +1,19 @@
+class Purchase_Process:
+
+ #"""Purchase_Process class will describe the objects of items needed to buy for
+ #the company (purchase process), that will be read from a .csv file with
+ #information as ProcessID, ProdName, Qty, DelTime, Loc."""
+
+	def __init__(self,Purchase_Process,Product_Name,Quantity_Required):
+		self.Purchase_Process=Purchase_Process
+		self.Product_Name=Product_Name
+		self.Quantity_Required=Quantity_Required
+		#self.Delivery_Time_Required
+		#self.Location_Required
+
+	def Show_data(self):
+		return (
+		"Purchase_Process "+str(self.Purchase_Process)+
+		"\nProduct_Name "+str(self.Product_Name)+
+		"\nQuantity_Required "+str(self.Quantity_Required)
+		)
---
+++ src/main/python/models/quotation.py
@@ -0,0 +1,29 @@
+class Quotation:
+
+ #""""Quotation" class will describe the objects of quotations sent by providers
+ #that could be registered (points) or not (no points),and will have all the
+ #information needed to score the quotation and classify it by Purchase_Process_ID"""
+
+	def __init__(self,Provider_Code,Purchase_Process,Product_Name,Quantity,Unit_Price,Delivery_Time):
+		self.Provider_Code=Provider_Code
+		self.Purchase_Process=Purchase_Process
+		self.Product_Name=Product_Name
+		self.Quantity=Quantity
+		self.Unit_Price=Unit_Price
+		self.Delivery_Time=Delivery_Time
+		#self.Delivery_Location=Delivery_Location
+		self.Total_Points=0
+		self.Rank=0
+
+	def Show_data(self):
+		return (
+		"Provider_Code "+str(self.Provider_Code)+
+		"\nPurchase_Process "+str(self.Purchase_Process)+
+		"\nProduct_Name "+str(self.Product_Name)+
+		"\nQuantity "+str(self.Quantity)+
+		"\nUnit_Price "+str(self.Unit_Price)+
+		"\nDelivery_Time "+str(self.Delivery_Time)+
+		#print(self.Delivery_Location)
+		"\nTotal_Points "+str(self.Total_Points)+
+		"\nRank "+str(self.Rank)
+		)

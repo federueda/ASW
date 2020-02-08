4209a32194dd9c84ffca18e32fcd7f369bf53637
FR <55557961+federueda@users.noreply.github.com>
2020-02-08 01:00:48 +0100

    Update pp_tests.py

--- src/unittest/python/pp_tests.py
+++ src/unittest/python/pp_tests.py
@@ -1,11 +1,1 @@
-#from models.purchase_process import Purchase_Process
 
-#pp = Purchase_Process(2222,"Monitor",20)
-
-# Confirm expected data
-
-#assert pp.Purchase_Process == 2222
-#assert pp.Product_Name == "Monitor"
-#assert pp.Quantity_Required = 20
-
-#assert pr.Show_data() == "Provider_ID 1111\nProvider_Name ComputersCo\nProvider_Address = Barranquilla\nLocal_Company True\nQuality_Cert False"
